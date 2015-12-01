a95de295b05e8e27a5cc16180cd1a1b8e99b95b8
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2015-12-01 11:37:05 -0700

    Initial Commit

---
+++ lib.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $( "button" ).click(function() {
+      $( "#getstarted" ).hide( "fast", function() {
+              $( "#messages" ).show( "fast", function() {
+      
+        });
+      });
+    });
+});
